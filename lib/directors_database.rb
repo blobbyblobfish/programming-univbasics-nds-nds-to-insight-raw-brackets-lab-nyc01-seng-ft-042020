1fda74adb88f513ee602bdb1947f4588ce73f675
Alice Wang <aywang6@gmail.com>
2020-03-29 15:44:53 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -2,5 +2,8 @@ require 'yaml'
 
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
+	puts "directors_db"
+	puts File.open("directors_db").read
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
